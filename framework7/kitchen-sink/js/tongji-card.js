c622d0e407752f2a7e19acaf251e69ce99eddbbe
Panqingxing <panqingxing.128@163.com>
2018-03-02 15:26:05 +0800

    1. 添加统计

---
+++ framework7/kitchen-sink/js/tongji-card.js
@@ -0,0 +1,11 @@
+  // 腾讯统计
+var _mtac = {"performanceMonitor":1};
+(function() {
+  var mta = document.createElement("script");
+  mta.src = "http://pingjs.qq.com/h5/stats.js?v2.0.4";
+  mta.setAttribute("name", "MTAH5");
+  mta.setAttribute("sid", "500510322");
+  mta.setAttribute("cid", "500510343");
+  var s = document.getElementsByTagName("script")[0];
+  s.parentNode.insertBefore(mta, s);
+})();
